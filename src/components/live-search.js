08309c2a13ed49ed752d002b343f85e5f2e89d66
Reed Bertolette <reed@bertopro.com>
2018-08-17 16:38:45 -1000

    Version that optimizes use of toLowerCase

--- src/components/live-search.js
+++ src/components/live-search.js
@@ -10,19 +10,33 @@ export default class LiveSearch extends React.Component {
         super(props);
 
         this.state = {
+            // init this to peaks, adding a property of
+            // lower case names so that they don't have to 
+            // toLowerCase doesn't have to be looped through
+            // all people every time the search value is changed
+            peaksLcName: peaks.map(person => ({
+                    name: person.name,
+                    description: person.description,
+                    actor: person.actor,
+                    nameLc: person.name.toLowerCase()
+                })
+            ),
+            // init to all characters, will be reset on every searchString change
             characters: peaks
         }
     }
-
+    const 
     findCharacters(findString) {
-        //console.log(!!findString.trim());
-        let displayCharacters = peaks;
+        // assume that the findstring is empty, fill w/ all characters
+        let displayCharacters = this.state.peaksLcName;
+
+        
         // !! is used to convert findString.trim() into a boolean,
         // false if an empty string, true if not empty
         if (!!findString.trim()) {
+            // init to be the lower case to avoid running toLowerCase() for every object
             const findStrToLower = findString.trim().toLowerCase();
-            // displayCharacters.filter(character => character.name.indexOf(findString) !== -1);
-            displayCharacters = displayCharacters.filter(obj => obj.name.toLowerCase().includes(findStrToLower));
+            displayCharacters = displayCharacters.filter(obj => obj.nameLc.includes(findStrToLower));
         }
 
         this.setState({
